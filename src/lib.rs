cfdff65b98895f61fd906838c6a3d5c65e662894
Paul Mason <paul@paulmason.me>
2021-05-03 15:45:58 +1200

    Added documentation for library and license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//! pg_query.rs &emsp; [![Build Status]][actions] [![Latest Version]][crates.io] [![Docs Badge]][docs]
+//! ===========
+//!
+//! [Build Status]: https://img.shields.io/endpoint.svg?url=https%3A%2F%2Factions-badge.atrox.dev%2Fpaupino%2Fpg_query.rs%2Fbadge&label=build&logo=none
+//! [actions]: https://actions-badge.atrox.dev/paupino/pg_query.rs/goto
+//! [Latest Version]: https://img.shields.io/crates/v/pg_query.rs.svg
+//! [crates.io]: https://crates.io/crates/pg_query.rs
+//! [Docs Badge]: https://docs.rs/pg_query.rs/badge.svg
+//! [docs]: https://docs.rs/pg_query.rs
+//!
+//! PostgreSQL parser that uses the [actual PostgreSQL server source]((https://github.com/pganalyze/libpg_query)) to parse
+//! SQL queries and return the internal PostgreSQL parse tree.
+//!
+//! Warning! This library is in early stages of development so any APIs exposed are subject to change.
+//!
+//! # Example: Parsing a query
+//!
+//! ```rust
+//! use pg_query::ast::Node;
+//!
+//! let result = pg_query::parse("SELECT * FROM contacts");
+//! assert!(result.is_ok());
+//! let result = result.unwrap();
+//! let el: &Node = &result[0];
+//! assert!(matches!(*el, Node::SelectStmt(_)));
+//! ```
+//!
 pub mod ast;
 mod bindings;
 mod error;
--- src/query.rs
+++ src/query.rs
@@ -13,14 +13,29 @@ struct ParseResult {
 #[derive(Debug, serde::Deserialize)]
 struct Stmt {
     stmt: crate::ast::Node,
-    stmt_len: u32,
+    stmt_len: Option<u32>,
 }
 
+/// Represents the resulting fingerprint containing both the raw integer form as well as the
+/// corresponding 16 character hex value.
 pub struct Fingerprint {
     pub value: u64,
     pub hex: String,
 }
 
+/// Parses the given SQL statement into the given abstract syntax tree.
+///
+/// # Example
+///
+/// ```rust
+/// use pg_query::ast::Node;
+///
+/// let result = pg_query::parse("SELECT * FROM contacts");
+/// assert!(result.is_ok());
+/// let result = result.unwrap();
+/// let el: &Node = &result[0];
+/// assert!(matches!(*el, Node::SelectStmt(_)));
+/// ```
 pub fn parse(stmt: &str) -> Result<Vec<crate::ast::Node>> {
     unsafe {
         let c_str = CString::new(stmt).unwrap();
@@ -43,6 +58,16 @@ pub fn parse(stmt: &str) -> Result<Vec<crate::ast::Node>> {
     }
 }
 
+/// Normalizes the given SQL statement, returning a parametized version.
+///
+/// # Example
+///
+/// ```rust
+/// let result = pg_query::normalize("SELECT * FROM contacts WHERE name='Paul'");
+/// assert!(result.is_ok());
+/// let result = result.unwrap();
+/// assert_eq!(result, "SELECT * FROM contacts WHERE name=$1");
+/// ```
 pub fn normalize(stmt: &str) -> Result<String> {
     unsafe {
         let c_str = CString::new(stmt).unwrap();
@@ -64,6 +89,17 @@ pub fn normalize(stmt: &str) -> Result<String> {
     }
 }
 
+/// Fingerprints the given SQL statement. Useful for comparing parse trees across different implementations
+/// of `libpg_query`.
+///
+/// # Example
+///
+/// ```rust
+/// let result = pg_query::fingerprint("SELECT * FROM contacts WHERE name='Paul'");
+/// assert!(result.is_ok());
+/// let result = result.unwrap();
+/// assert_eq!(result.hex, "0e2581a461ece536");
+/// ```
 pub fn fingerprint(stmt: &str) -> Result<Fingerprint> {
     unsafe {
         let c_str = CString::new(stmt).unwrap();
@@ -88,6 +124,25 @@ pub fn fingerprint(stmt: &str) -> Result<Fingerprint> {
     }
 }
 
+/// An experimental API which parses a PLPGSQL function. This currently returns the raw JSON structure.
+///
+/// # Example
+///
+/// ```rust
+/// let result = pg_query::parse_plpgsql(
+///         " \
+///         CREATE OR REPLACE FUNCTION cs_fmt_browser_version(v_name varchar, v_version varchar) \
+///         RETURNS varchar AS $$ \
+///         BEGIN \
+///             IF v_version IS NULL THEN \
+///                 RETURN v_name; \
+///             END IF; \
+///             RETURN v_name || '/' || v_version; \
+///         END; \
+///         $$ LANGUAGE plpgsql;",
+///     );
+/// assert!(result.is_ok());
+/// ```
 pub fn parse_plpgsql(stmt: &str) -> Result<serde_json::Value> {
     unsafe {
         let c_str = CString::new(stmt).unwrap();
