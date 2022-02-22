b1c9bb28e2dff20477883942123a856843de0ef7
Alex Pearson <alex@alexpear.com>
2022-02-22 16:17:07 -0500

    use proper OUT_DIR for faster rebuilds

--- build.rs
+++ build.rs
@@ -1,30 +1,63 @@
 #![cfg_attr(feature = "clippy", feature(plugin))]
 #![cfg_attr(feature = "clippy", plugin(clippy))]
 
+use fs_extra::dir::CopyOptions;
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 
-fn main() {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+static SOURCE_DIRECTORY: &str = "libpg_query";
+static LIBRARY_NAME: &str = "pg_query";
 
-    println!("cargo:rerun-if-changed=./libpg_query/Makefile"); // Includes version number
-    println!("cargo:rustc-link-search=native=./libpg_query");
-    println!("cargo:rustc-link-lib=static=pg_query");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    let build_path = Path::new(".").join(SOURCE_DIRECTORY);
+    let makefile_path = build_path.join("Makefile");
+    let out_header_path = out_dir.join(LIBRARY_NAME).with_extension("h");
+    let out_protobuf_path = out_dir.join("protobuf");
+
+    // Configure cargo through stdout
+    println!("cargo:rerun-if-changed={}", makefile_path.display()); // Includes version number
+    println!("cargo:rustc-link-search=native={}", build_path.display());
+    println!("cargo:rustc-link-lib=static={}", LIBRARY_NAME);
+
+    // Copy the relevant source files to the OUT_DIR
+    let source_paths = vec![
+        build_path.join(LIBRARY_NAME).with_extension("h"),
+        build_path.join("Makefile"),
+        build_path.join("src"),
+        build_path.join("protobuf"),
+        build_path.join("vendor"),
+    ];
+
+    let copy_options = CopyOptions { overwrite: true, ..CopyOptions::default() };
+
+    fs_extra::copy_items(&source_paths, &out_dir, &copy_options)?;
 
     // Compile the C library.
     let mut make = Command::new("make");
-    make.current_dir("libpg_query");
+
+    make.env_remove("PROFILE").arg("-C").arg(&out_dir).arg("build");
+
     if env::var("PROFILE").unwrap() == "debug" {
         make.arg("DEBUG=1");
     }
-    let status = make.stdin(Stdio::null()).stdout(Stdio::inherit()).stderr(Stdio::inherit()).status().unwrap();
-    assert!(status.success());
+
+    let status = make.stdin(Stdio::null()).stdout(Stdio::inherit()).stderr(Stdio::inherit()).status()?;
+
+    if !status.success() {
+        return Err("libpg_query compilation failed".into());
+    }
 
     // Generate bindings for Rust
-    let bindings = bindgen::Builder::default().header("./libpg_query/pg_query.h").generate().expect("Unable to generate bindings");
-    bindings.write_to_file(out_dir.join("bindings.rs")).expect("Couldn't write bindings!");
+    bindgen::Builder::default()
+        .header(out_header_path.to_str().ok_or("Invalid header path")?)
+        .generate()
+        .map_err(|_| "Unable to generate bindings")?
+        .write_to_file(out_dir.join("bindings.rs"))?;
 
     // Generate the protobuf definition
-    prost_build::compile_protos(&["./libpg_query/protobuf/pg_query.proto"], &["./libpg_query/protobuf"]).expect("protobuf generation failed");
+    prost_build::compile_protos(&[&out_protobuf_path.join(LIBRARY_NAME).with_extension("proto")], &[&out_protobuf_path])?;
+
+    Ok(())
 }
